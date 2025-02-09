99d96a3b55b75fe7294dc02c42c1efb3bd88917f
PhiBee <phibidossantos@gmail.com>
2025-02-09 20:01:29 +0100

    Added clean function for sales DataFrame and what needs to eb done in it still

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,6 +6,8 @@ from os.path import isfile, join
 utf16_encoded_files = ["reviews", "stats_crashes", "stats_ratings_country", "stats_ratings_overview"]
 utf8_encoded_files = ["sales"]
 
+product_id = "com.vansteinengroentjes.apps.ddfive"
+
 # Should return a pandas dataframe with the data from the csv files in that dir
 def get_data_from_csv(data_type: str):
     path_to_data = "data/"
@@ -47,7 +49,7 @@ def get_data_from_csv(data_type: str):
             final_df = pd.concat(data_frames, ignore_index=True)
             return final_df 
 
-def get_data_from_csv_cleaner(data_type: str):
+def get_data_from_csv_cleaner(data_type: str) -> pd.DataFrame:
     path_to_data = "data/"
     if data_type in utf16_encoded_files:
         # Add the specific folder to the path
@@ -67,3 +69,23 @@ def get_data_from_csv_cleaner(data_type: str):
         final_df = pd.concat(data_frames, ignore_index=True)
         
         return final_df 
+    
+def clean_sales(df: pd.DataFrame) -> pd.DataFrame:
+    # Filter to only the app we want
+    df = df[df['Product id'] == product_id]
+    # Filter out anything that's not a Charge
+    df = df[df['Transaction Type'] == "Charge"]
+    
+    # Hardware = Device Model
+    # Sku Id = SKU ID
+    # Order Number = Description
+    # Transaction Date = Order Charged Date
+    # Product id = Product ID
+    # Product Type (0 = paidapp, 1 = inapp)
+    # State of Buyer = Buyer State
+    # Country of Buyer = Buyer Country
+    # Postal Code of Buyer = Buyer Postal Code
+    # Transaction Type = Financial Status (Charged = Charge)
+    # Currency of Sale = Buyer Currency
+    # Charged Amount = Amount (Buyer Currency)
+    return df
--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from data_preprocessing import *
 
 def main():
-    df = get_data_from_csv_cleaner("reviews")
+    df = get_data_from_csv_cleaner("sales")
     print(df)
-    df = get_data_from_csv("sales")
-    print(df)
-    df = get_data_from_csv("stats_crashes")
-    print(df)
-    df = get_data_from_csv("stats_ratings_country")
-    print(df)
-    df = get_data_from_csv("stats_ratings_overview")
-    print(df)
+    df = clean_sales(df)
+    print(df.columns)
 
 if __name__ == "__main__":
     main()
