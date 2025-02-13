faf768e81bdac1d27ba4d61f5d5721c1232b1f8f
Lars Brussee <brussee.lars@gmail.com>
2025-02-13 13:55:15 +0100

    My shitty code for a part of the code

--- src/to_html.py
+++ src/to_html.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from bokeh.plotting import figure, show
 from bokeh.io import curdoc
-from bokeh.models import DatetimeTickFormatter
+from bokeh.models import DatetimeTickFormatter, ColumnDataSource, CDSView, GroupFilter
 
 def sales_volume(df: pd.DataFrame):
     # TODO: Should create a visual of sales over time in terms of at least two measures
@@ -21,7 +21,15 @@ def sales_volume(df: pd.DataFrame):
     plot.xaxis[0].formatter = DatetimeTickFormatter(months="%b %Y")
 
 def ratings_and_stability(df: pd.DataFrame):
-    # TODO: Key performance indicators to understand stability related to ratings
+    # TODO: Key performance indicators to understand stability related to ratings, This is Lars task now.
+    ratings_cds = ColumnDataSource(df)
+    
+    daily_crashes_view = CDSView(source=ratings_cds, filters=[GroupFilter(column_name='Daily Crashes', group='IDK')])#TODO: probebly want to preselect some of the data for this. Usefull columns are the date, daily & total average rating, perhaps per country?, and daily crashes and ANRs
+    daily_ratings_view = CDSView(source=ratings_cds, filters=[])
+    plot = figure(
+	title="ratings and stability TEST",
+	
+    )
     pass
 
 def geographical_view(df: pd.DataFrame):
